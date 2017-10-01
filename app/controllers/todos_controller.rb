0d0bdad1373ea936757cc8cfda6dba18ab7030a7
Student <gia0320@gmail.com>
2017-10-01 14:03:53 +0000

    created-database

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,4 +1,26 @@
 class TodosController < ApplicationController
     def index
+        @code_word="#{params[:code]} #{params[:word]}"
+    end
+        #{params[:word]}"
+    def show
+        id = params[:id]
+        if id=='1'
+            @todo={
+                id:id,
+                name: 'Do homework',
+                duration: 60
+            }
+        elsif id=='2'
+        
+        @todo={
+            id: params[:id],
+            name: 'Wake up at 8am',
+            description: 'Mon to Fri',
+            duration: 15
+            }
+        else
+            @todo={}
+        end
     end
 end
