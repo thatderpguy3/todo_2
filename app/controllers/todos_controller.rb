b7e93fb1360a89d1d75c33976a6c81e5f0d74d03
Student <gia0320@gmail.com>
2017-10-01 16:17:02 +0000

    created-database

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,4 +1,13 @@
 class TodosController < ApplicationController
+    def create
+      todo=Todo.create(
+           name: params[:name],
+           description: params[:description],
+           duration: params[:duration],
+           complete: params[:complete]
+           ) 
+           redirect_to "/todos/#{todo.id}"
+    end
     def index
         @code_word="#{params[:code]} #{params[:word]}"
     end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root 'todos#index'
   get 'todos' => 'todos#index'
   get "todos/:id" => 'todos#show'
+  post 'todos' => 'todos#create'
 end
